d9818d4709765b00c47883c30731993d3206eac2
Admin <eleckiyvladimir@gmail.com>
2020-03-03 20:05:26 +0200

    initial commit

---
+++ app/src/main/java/com/example/activity_task/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.activity_task;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button submit;
+    EditText name, surname, age;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        submit = findViewById(R.id.button);
+        name = findViewById(R.id.Name);
+        surname = findViewById(R.id.Surname);
+        age = findViewById(R.id.Age);
+
+        submit.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, second_Activity.class);
+        //intent.putExtra("send_name", name.getText().toString());
+        //intent.putExtra("send_surname", surname.getText().toString());
+        //intent.putExtra("send_age", Integer.parseInt(age.getText().toString()));
+        startActivity(intent);
+    }
+
+}
+
--- app/src/main/java/com/example/activity_task/second_Activity.java
+++ app/src/main/java/com/example/activity_task/second_Activity.java
@@ -3,12 +3,35 @@ package com.example.activity_task;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.widget.TextView;
 
 public class second_Activity extends AppCompatActivity {
+    TextView text1, text2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_);
+
+        text1.findViewById(R.id.textView1);
+        text2.findViewById(R.id.textView2);
+
+       /* Intent intent = getIntent();
+
+        String name = intent.getStringExtra("send_name");
+        String surname = intent.getStringExtra("send_surname");
+        int age = intent.getIntExtra("send_age", 0);
+
+        text1.setText("Hello " + name + " " + surname);
+
+        if(age < 18)
+        {
+            text2.setText("Sorry, but your age less than 18");
+        }
+        else
+        {
+            text2.setText("Your age 18 or more, so you can watch our content");
+        }*/
     }
 }
